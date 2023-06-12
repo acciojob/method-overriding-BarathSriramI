998e89bf5928334696e347d5575679ac999d7bbe
barath sriram <barathsriramiyyappan@gmail.com>
2023-06-12 10:48:39 +0530

    added solutions

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -3,7 +3,7 @@ package com.driver;
 public class A {
 
 
-    public static String meth ()
+    String meth ()
     {
         return  "Invoking method from class A";
     }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,15 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+
+        B b= new B();
+        b.meth();
+
+        // overriden
+
         B a= new B();
         a.meth();
+//        System.out.println(a.meth());
 
     }
 }
